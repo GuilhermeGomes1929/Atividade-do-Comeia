2cb84376bb40749496210b67c64b31cd7a943051
GuilhermeGomes1929 <guilherme_estudos@outlook.com>
2021-08-25 21:35:53 -0300

    Folder structure and dependencies

--- app/src/main/java/com/atividade/comeia/activity/MainActivity.java
+++ app/src/main/java/com/atividade/comeia/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.atividade.comeia;
+package com.atividade.comeia.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.atividade.comeia.R;
 
 public class MainActivity extends AppCompatActivity {
 
