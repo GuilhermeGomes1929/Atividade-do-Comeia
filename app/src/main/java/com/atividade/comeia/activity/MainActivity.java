9de988e12d0b837339050e5491fec8dd1a1a3e92
GuilhermeGomes1929 <guilherme_estudos@outlook.com>
2021-08-27 12:05:31 -0300

    bugs fixed

--- app/src/main/java/com/atividade/comeia/activity/MainActivity.java
+++ app/src/main/java/com/atividade/comeia/activity/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSupportActionBar(findViewById(R.id.toolbar));
-
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         setupSearchBtn();
 
